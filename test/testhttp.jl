adc4239c66ac477c0aa0da51390e2c8344d23f74
dotslashb <dotslashb@gmail.com>
2014-04-12 12:03:34 -0400

    cleanup

--- test/testhttp.jl
+++ test/testhttp.jl
@@ -1,5 +1,3 @@
-include("../src/ConfParser.jl")
-
 using ConfParser
 
 conf = ConfParse("config.http")
@@ -7,15 +5,4 @@ parse_conf!(conf)
 
 email    = param(conf, "email")
 password = param(conf, "password")
-
-# gets multiple values seperated by comma
 foobars  = param(conf, "foobars")
-
-println("Email: $email Password: $password")
-
-# print values in foobars line
-println("Foobars:")
-for value = foobars
-    println("- $value")
-end
-
--- test/testsimple.jl
+++ test/testsimple.jl
@@ -1,5 +1,3 @@
-include("../src/ConfParser.jl")
-
 using ConfParser
 
 conf = ConfParse("config.simple")
@@ -8,6 +6,3 @@ parse_conf!(conf)
 protocol = param(conf, "protocol")
 port     = param(conf, "port")
 user     = param(conf, "user")
-
-println("Protocol: $protocol Port: $port")
-println("User: $user")
